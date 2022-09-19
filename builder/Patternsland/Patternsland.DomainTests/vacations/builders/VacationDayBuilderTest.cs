6b1eaf3f3ab876ee592342424686937ad6997437
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-19 18:31:56 +0100

    builder: Patternsland: add unit test to cover multiple reservations for a day

--- builder/Patternsland/Patternsland.DomainTests/vacations/builders/VacationDayBuilderTest.cs
+++ builder/Patternsland/Patternsland.DomainTests/vacations/builders/VacationDayBuilderTest.cs
@@ -44,4 +44,44 @@ public class VacationDayBuilderTest
     Assert.Equal(createdReservation.Place.Name, parkReservation.Place.Name);
     Assert.Equal(createdReservation.ReservationDate, parkReservation.ReservationDate);
   }
+
+  [Fact]
+  public void TestBuildsVacationDayWithMultipleReservations()
+  {
+    var date = DateTime.Now;
+    var builder = new VacationDayBuilder();
+    var parkReservation = new Reservation(
+      reservationDate: date,
+      ownerIdentification: "1234",
+      place: new Park(name: "Patternsland")
+    );
+
+    var hotelReservation = new Reservation(
+      reservationDate: date,
+      ownerIdentification: "1234",
+      place: new Hotel("Grand Facadian")
+    );
+
+    builder.SetDate(date);
+    builder.AddReservation(parkReservation);
+    builder.AddReservation(hotelReservation);
+
+    var vacationDay = builder.GetVacationDay();
+
+    var reservationForVacationDay = vacationDay.GetReservationsFor(date);
+
+    Assert.Collection(
+      reservationForVacationDay,
+      firstReservation =>
+      {
+        Assert.Equal(firstReservation.Place.Name, parkReservation.Place.Name);
+        Assert.Equal(firstReservation.ReservationDate, parkReservation.ReservationDate);
+      },
+      secondReservation =>
+      {
+        Assert.Equal(secondReservation.Place.Name, hotelReservation.Place.Name);
+        Assert.Equal(secondReservation.ReservationDate, hotelReservation.ReservationDate);
+      }
+    );
+  }
 }
