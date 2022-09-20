9b55d56f811159438ca06a5fd7c95e272b1a7da0
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-20 15:42:49 +0100

    builder: Patternsland: implement sample usage for the builders

--- builder/Patternsland/Patternsland.Console/Program.cs
+++ builder/Patternsland/Patternsland.Console/Program.cs
@@ -6,19 +6,38 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    var date = DateTime.Now;
+    var firstDay = new DateTime(2022, 9, 20);
     var ownerIdentification = "PP1234";
     var park = new Park(name: "Patternsland");
+    var restaurant = new Restaurant(name: "Objectville's Diner");
     var hotel = new Hotel(name: "Grand Facadian");
-    var reservations = new List<Reservation>()
+    var reservationsForFirstDay = new List<Reservation>()
     {
-      new Reservation(date, ownerIdentification, place: park),
-      new Reservation(date, ownerIdentification, place: hotel),
+      new Reservation(firstDay, ownerIdentification, place: hotel),
+      new Reservation(firstDay, ownerIdentification, place: park),
+    };
+
+    var secondDay = new DateTime(2022, 9, 21);
+    var reservationsForSecondDay = new List<Reservation>()
+    {
+      new Reservation(firstDay, ownerIdentification, place: hotel),
+      new Reservation(firstDay, ownerIdentification, place: restaurant),
     };
 
     var builder = new VacationBuilder();
-    builder.AddReservationsForDay(date, reservations);
+    builder.AddReservationsForDay(firstDay, reservationsForFirstDay);
+    builder.AddReservationsForDay(secondDay, reservationsForSecondDay);
 
     var planner = builder.GetVacationPlanner();
+
+    Console.WriteLine($"Vacation days planned: {planner.VacationDays.Count}");
+    planner.VacationDays.ToList().ForEach(day =>
+    {
+      Console.WriteLine($"\t{day.Date.ToShortDateString()}: {day.Reservations.Count} reservations");
+      day.Reservations.ToList().ForEach(reservation =>
+      {
+        Console.WriteLine($"\t\tPlace: {reservation.Place.Name} | Owner identification: {reservation.OwnerIdentification}");
+      });
+    });
   }
 }
