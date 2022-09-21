4ab842805bd3d73b938743af04c160785b4c337a
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-21 12:52:46 +0100

    chain-of-responsibility: MightyGumballMailing - move Email to its own file

---
+++ chain-of-responsibility/MightyGumballMailing/MightyGumballMailing.Domain/Email.cs
@@ -0,0 +1,13 @@
+namespace MightyGumballMailing.Domain;
+
+public class Email
+{
+  public string Id { get; private set; }
+  public EmailType Type { get; private set; }
+
+  public Email(string id, EmailType type, string subject, string body)
+  {
+    this.Id = id;
+    this.Type = type;
+  }
+}
