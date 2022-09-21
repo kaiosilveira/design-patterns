4888f8404ff836bbb5071c5db8a8dfc1d59a3b02
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-21 14:06:04 +0100

    chain-of-responsibility: MightyGumballMailing - add ForwardToCEO method to MailService interface

--- chain-of-responsibility/MightyGumballMailing/MightyGumballMailing.Domain/services/MailService.cs
+++ chain-of-responsibility/MightyGumballMailing/MightyGumballMailing.Domain/services/MailService.cs
@@ -4,6 +4,7 @@ namespace MightyGumballMailing.Domain.Services;
 
 public interface MailService
 {
+  void ForwardToCEO(Email email);
   void ForwardToLegalDepartment(Email email);
   void MoveToJunk(string emailId);
 }
