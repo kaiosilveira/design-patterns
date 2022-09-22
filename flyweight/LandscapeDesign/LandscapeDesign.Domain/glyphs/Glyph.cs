18682df2ce1291f21699c1a5aad99fbd34909fdc
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-22 15:10:25 +0100

    flyweight: LandscapeDesign - move Glyph to its own file

---
+++ flyweight/LandscapeDesign/LandscapeDesign.Domain/glyphs/Glyph.cs
@@ -0,0 +1,40 @@
+namespace LandscapeDesign.Domain;
+
+public class Glyph
+{
+  private readonly string character;
+  private readonly string unicodeValue;
+  private string name;
+
+  public Glyph(string character, string unicodeValue, string name)
+  {
+    this.character = character;
+    this.unicodeValue = unicodeValue;
+    this.name = name;
+  }
+
+  public void Display(int x, int y, Window window)
+  {
+    window.Add(x, y, this.character);
+  }
+
+  public virtual int GetHeight()
+  {
+    return 1;
+  }
+
+  public virtual string GetChar()
+  {
+    return this.character;
+  }
+
+  public virtual string GetUnicodeValue()
+  {
+    return this.unicodeValue;
+  }
+
+  public virtual string GetName()
+  {
+    return this.name;
+  }
+}
