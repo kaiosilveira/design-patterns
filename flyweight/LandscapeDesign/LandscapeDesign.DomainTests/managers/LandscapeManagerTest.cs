ba0a6fd8ab48db1dd725b3c33435863198dc5611
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-22 15:26:02 +0100

    flyweight: LandscapeDesign - add unit test for empty drawing schemes

--- flyweight/LandscapeDesign/LandscapeDesign.DomainTests/managers/LandscapeManagerTest.cs
+++ flyweight/LandscapeDesign/LandscapeDesign.DomainTests/managers/LandscapeManagerTest.cs
@@ -4,19 +4,19 @@ using LandscapeDesign.Domain.Managers;
 
 namespace LandscapeDesign.DomainTests;
 
-public class UnitTest1
+public class LandscapeManagerTest
 {
+  [Fact]
+  public void TestReturnsAnEmptyMatrixIfNothingWasAdded()
+  {
+    var manager = new LandscapeManager(xSize: 1, ySize: 1);
+    var scheme = manager.GetDrawingScheme();
+    Assert.Null(scheme[0][0]);
+  }
+
   [Fact]
   public void TestConvertsGlyphToUnsharedIfCustomPropertiesAreAdded()
   {
-    string[][] expected = {
-      new string[] { "🌳", "🟫", "🌳", "🟫", "🌳" },
-      new string[] { "🌳", "⬜", "⬜", "⬜", "🟫" },
-      new string[] { "🌳", "⬜", "🏠", "⬜", "🌳" },
-      new string[] { "🌳", "⬜", "⬜", "⬜", "🟫" },
-      new string[] { "🌳", "🟫", "🌳", "🟫", "🌳" },
-    };
-
     var landscapeManager = new LandscapeManager(xSize: 5, ySize: 5);
 
     landscapeManager.Add(0, 0, GlyphRegistry.GLYPHS["🌳"]);
