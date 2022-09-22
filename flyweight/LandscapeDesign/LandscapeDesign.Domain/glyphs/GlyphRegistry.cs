d7e54fa5e34ce841b7f0b2bbc2e4f458b5a4890f
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-22 15:10:46 +0100

    flyweight: LandscapeDesign - move GlyphRegistry to its own file

---
+++ flyweight/LandscapeDesign/LandscapeDesign.Domain/glyphs/GlyphRegistry.cs
@@ -0,0 +1,12 @@
+namespace LandscapeDesign.Domain;
+
+public static class GlyphRegistry
+{
+  public static Dictionary<string, Glyph> GLYPHS = new Dictionary<string, Glyph>()
+  {
+    { "🟫", new Glyph(character: "🟫", unicodeValue: "U+1F7EB", name: "Brown Square") },
+    { "⬜", new Glyph(character: "⬜", unicodeValue: "U+2B1C", name: "White Square") },
+    { "🌳", new Glyph(character: "🌳", unicodeValue: "U+1F333", name: "Tree") },
+    { "🏠", new Glyph(character: "🏠", unicodeValue: "U+1F3E0", name: "House") },
+  };
+}
