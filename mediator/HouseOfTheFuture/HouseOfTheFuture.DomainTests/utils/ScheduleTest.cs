898b29ffa25cdff6c6e2c1e79c8b5f71ef34c3c4
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-24 17:35:18 +0100

    mediator: HouseOfTheFuture - introduce Schedule class

---
+++ mediator/HouseOfTheFuture/HouseOfTheFuture.DomainTests/utils/ScheduleTest.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using HouseOfTheFuture.Domain.Utils;
+
+public class ScheduleTest
+{
+  [Fact]
+  public void TestSetup()
+  {
+    var hour = 7;
+    var minute = 0;
+    var second = 0;
+    var daysOfWeek = new List<DayOfWeek>() { DayOfWeek.Saturday, DayOfWeek.Sunday };
+    var schedule = new Schedule(daysOfWeek, hour, minute, second);
+
+    Assert.Equal(hour, schedule.ScheduledHour);
+    Assert.Equal(minute, schedule.ScheduledMinute);
+    Assert.Equal(daysOfWeek, schedule.ScheduledDaysOfWeek);
+  }
+
+  [Fact]
+  public void TestMatch()
+  {
+    var hour = 7;
+    var minute = 0;
+    var second = 0;
+    var daysOfWeek = new List<DayOfWeek>() { DayOfWeek.Saturday, DayOfWeek.Sunday };
+
+    var schedule = new Schedule(daysOfWeek, hour, minute, second);
+
+    var scheduledDateTime = new DateTime(year: 2022, month: 9, day: 24, hour, minute, second);
+    Assert.True(schedule.Matches(scheduledDateTime));
+
+    var unscheduledDateTime = new DateTime(year: 2022, month: 9, day: 21, hour, minute, second);
+    Assert.False(schedule.Matches(unscheduledDateTime));
+  }
+}
