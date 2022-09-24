5d74c11acd93284a04c9bc24892cfb5cb0928c15
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-24 11:43:35 +0100

    interpreter: BoolExParser - add working example

--- interpreter/BoolExParser/BoolExParser.ConsoleApp/Program.cs
+++ interpreter/BoolExParser/BoolExParser.ConsoleApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BoolExParser.Domain;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var context = new InMemoryLanguageContext();
+    var x = new VariableExp('x');
+    var y = new VariableExp('y');
+
+    Console.WriteLine($"Expression: (true and x) or (y and not x)");
+
+    Console.Write("x: ");
+    var xValue = Convert.ToBoolean(Console.ReadLine());
+    context.Assign('x', xValue);
+
+    Console.Write("y: ");
+    var yValue = Convert.ToBoolean(Console.ReadLine());
+    context.Assign('y', yValue);
+
+    var expression = new OrExp(new AndExp(new Constant(true), x), new AndExp(y, new NotExp(x)));
+    var result = expression.Evaluate(context);
+
+    Console.WriteLine($"Evaluates to: {result} (x={xValue}, y={yValue})");
+  }
+}
