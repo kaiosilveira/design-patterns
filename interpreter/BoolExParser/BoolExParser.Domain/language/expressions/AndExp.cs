b683076f827fdee03aac26e859113f17144e87a7
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-24 07:50:27 +0100

    interpreter: BoolExParser - move AndExp to its own file

---
+++ interpreter/BoolExParser/BoolExParser.Domain/language/expressions/AndExp.cs
@@ -0,0 +1,28 @@
+namespace BoolExParser.Domain;
+
+public class AndExp : BooleanExp
+{
+  private BooleanExp operand1;
+  private BooleanExp operand2;
+
+  public AndExp(BooleanExp operand1, BooleanExp operand2)
+  {
+    this.operand1 = operand1;
+    this.operand2 = operand2;
+  }
+
+  public BooleanExp Copy()
+  {
+    return new AndExp(operand1.Copy(), operand2.Copy());
+  }
+
+  public bool Evaluate(Context context)
+  {
+    return operand1.Evaluate(context) && operand2.Evaluate(context);
+  }
+
+  public BooleanExp Replace(char c, BooleanExp exp)
+  {
+    return new AndExp(operand1.Replace(c, exp), operand2.Replace(c, exp));
+  }
+}
