be7da12b91adc850a16ced52fcc48c2afcd2f29f
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-24 07:51:08 +0100

    interpreter: BoolExParser - move OrExp to its own file

---
+++ interpreter/BoolExParser/BoolExParser.Domain/language/expressions/OrExp.cs
@@ -0,0 +1,28 @@
+namespace BoolExParser.Domain;
+
+public class OrExp : BooleanExp
+{
+  private BooleanExp operand1;
+  private BooleanExp operand2;
+
+  public OrExp(BooleanExp operand1, BooleanExp operand2)
+  {
+    this.operand1 = operand1;
+    this.operand2 = operand2;
+  }
+
+  public BooleanExp Copy()
+  {
+    return new AndExp(operand1.Copy(), operand2.Copy());
+  }
+
+  public bool Evaluate(Context context)
+  {
+    return operand1.Evaluate(context) || operand2.Evaluate(context);
+  }
+
+  public BooleanExp Replace(char c, BooleanExp exp)
+  {
+    return new OrExp(operand1.Replace(c, exp), operand2.Replace(c, exp));
+  }
+}
