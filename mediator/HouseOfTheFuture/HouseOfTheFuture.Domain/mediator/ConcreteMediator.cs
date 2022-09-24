91c2d549667b82583e5a135a9a6cf987a9c0fe61
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-24 17:03:45 +0100

    mediator: HouseOfTheFuture - extract function to handle clock tick at ConcreteMediator

--- mediator/HouseOfTheFuture/HouseOfTheFuture.Domain/mediator/ConcreteMediator.cs
+++ mediator/HouseOfTheFuture/HouseOfTheFuture.Domain/mediator/ConcreteMediator.cs
@@ -15,17 +15,22 @@ public class ConcreteMediator : Mediator
     switch (e.Type)
     {
       case ApplicationEventType.CLOCK_TICK:
-        this.widgets
-          .Where(w => w.GetWidgetType() == WidgetType.ALARM)
-          .ToList()
-          .ForEach(alarm =>
-          {
-            var parsedDataObj = e.Data ?? DateTime.Now;
-            ((Alarm)alarm).CheckTime((DateTime)parsedDataObj);
-          });
+        HandleClockTick(e);
         break;
       default:
         break;
     }
+  }
+
+  private void HandleClockTick(ApplicationEvent e)
+  {
+    this.widgets
+      .Where(w => w.GetWidgetType() == WidgetType.ALARM)
+      .ToList()
+      .ForEach(alarm =>
+      {
+        var parsedDataObj = e.Data ?? DateTime.Now;
+        ((Alarm)alarm).CheckTime((DateTime)parsedDataObj);
+      });
   }
 }
