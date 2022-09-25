ecca8a4fa1477d8102e1a36f7a4a344acefcf1d7
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-25 11:20:57 +0100

    mediator: HouseOfTheFuture - introduce WidgetNotRegisteredException

---
+++ mediator/HouseOfTheFuture/HouseOfTheFuture.Domain/exceptions/WidgetNotRegisteredException.cs
@@ -0,0 +1,15 @@
+using HouseOfTheFuture.Domain.Widgets;
+
+public class WidgetNotRegisteredException : Exception
+{
+  private static Dictionary<WidgetType, string> widgetNames = new Dictionary<WidgetType, string>()
+  {
+    { WidgetType.COFFEE_POT, "Coffee Pot" },
+    { WidgetType.ALARM, "Alarm" },
+    { WidgetType.CLOCK, "Clock" },
+  };
+
+  public WidgetNotRegisteredException(WidgetType type)
+    : base(message: $"No widgets of type '{WidgetNotRegisteredException.widgetNames[type]}' were found. Have you tried registering them first?")
+  { }
+}
