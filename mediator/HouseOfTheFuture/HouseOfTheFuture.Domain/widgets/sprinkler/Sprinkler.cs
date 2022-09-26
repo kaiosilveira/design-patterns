72f62a46636856632b60870bb7b3cd846614bd34
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-26 10:55:34 +0100

    mediator: HouseOfTheFuture - make Sprinkler clock-dependent

--- mediator/HouseOfTheFuture/HouseOfTheFuture.Domain/widgets/sprinkler/Sprinkler.cs
+++ mediator/HouseOfTheFuture/HouseOfTheFuture.Domain/widgets/sprinkler/Sprinkler.cs
@@ -2,9 +2,10 @@ using HouseOfTheFuture.Domain.Utils;
 
 namespace HouseOfTheFuture.Domain.Widgets;
 
-public abstract class Sprinkler : Widget
+public abstract class Sprinkler : ClockDependentWidget
 {
   public abstract void CheckTime(DateTime time);
+
   public abstract string Describe();
 
   public abstract void SetSchedule(Schedule schedule);
