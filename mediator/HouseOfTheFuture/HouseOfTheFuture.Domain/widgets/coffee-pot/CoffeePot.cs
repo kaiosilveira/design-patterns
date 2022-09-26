09cb8555a1f9710c0cb32c78dfb1fda315ffda17
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-26 12:05:20 +0100

    mediator: HouseOfTheFuture - move methods inside CoffeePot interface

--- mediator/HouseOfTheFuture/HouseOfTheFuture.Domain/widgets/coffee-pot/CoffeePot.cs
+++ mediator/HouseOfTheFuture/HouseOfTheFuture.Domain/widgets/coffee-pot/CoffeePot.cs
@@ -2,11 +2,12 @@ namespace HouseOfTheFuture.Domain.Widgets;
 
 public abstract class CoffeePot : ClockDependentWidget
 {
+  public abstract void StartBrewing();
+
+  public abstract void CheckTime(DateTime time);
+
   public WidgetType GetWidgetType()
   {
     return WidgetType.COFFEE_POT;
   }
-
-  public abstract void StartBrewing();
-  public abstract void CheckTime(DateTime time);
 }
