221277646b8b348f6f009f102891fa9f57875d78
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-27 12:37:28 +0100

    memento: TicTacToe - move classes to domain

--- memento/TicTacToe/TicTacToe.DomainTests/UnitTest1.cs
+++ memento/TicTacToe/TicTacToe.DomainTests/UnitTest1.cs
@@ -1,46 +1,7 @@
 using Xunit;
+using TicTacToe.Domain;
 
 namespace TicTacToe.DomainTests;
-
-class BoardState
-{
-  public int CurrentPlayNumber { get; private set; }
-  public string?[] PlayedPositions { get; private set; }
-
-  public BoardState(int currentPlayNumber, string?[] playedPositions)
-  {
-    this.CurrentPlayNumber = currentPlayNumber;
-    this.PlayedPositions = playedPositions;
-  }
-}
-
-class Board
-{
-  public int CurrentPlayNumber { get; private set; }
-  public string?[] PlayedPositions { get; private set; }
-
-  public Board()
-  {
-    PlayedPositions = new string?[9];
-  }
-
-  public void AddPlay(int position, string symbol)
-  {
-    CurrentPlayNumber++;
-    PlayedPositions[position] = symbol;
-  }
-
-  public BoardState GetState()
-  {
-    return new BoardState(CurrentPlayNumber, PlayedPositions);
-  }
-
-  public void SetState(BoardState state)
-  {
-    this.CurrentPlayNumber = state.CurrentPlayNumber;
-    this.PlayedPositions = state.PlayedPositions;
-  }
-}
 
 public class BoardStateTest
 {
