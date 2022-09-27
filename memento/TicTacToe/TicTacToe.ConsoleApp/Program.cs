4c1c0b75ab82a104076b614552bb7eeff3994e14
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-27 15:31:28 +0100

    memento: TicTacToe - update console app code

--- memento/TicTacToe/TicTacToe.ConsoleApp/Program.cs
+++ memento/TicTacToe/TicTacToe.ConsoleApp/Program.cs
@@ -1,6 +1,23 @@
-﻿public class Program
+﻿using TicTacToe.Domain;
+
+public class Program
 {
   public static void Main(string[] args)
   {
+    var game = new Game(board: new InMemoryBoard());
+    var input = "";
+    while (input != "q!")
+    {
+      if (input?.Length == 2 && input[0] == 'p')
+      {
+        var position = Convert.ToInt32(input?[1].ToString());
+        game.ComputePlay(playedPosition: position);
+      }
+
+      if (input?.ToLower() == "u") game.Undo();
+
+      game.Draw();
+      input = Console.ReadLine();
+    }
   }
 }
