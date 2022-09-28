d14168f9bb67004e3ab847fc28525a58ecc6f049
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-28 08:42:40 +0100

    visitor: ObjectvilleFood - update console code to display health score info

--- visitor/ObjectvilleFood/ObjectvilleFood.Kiosk/Program.cs
+++ visitor/ObjectvilleFood/ObjectvilleFood.Kiosk/Program.cs
@@ -2,6 +2,7 @@
 using ObjectvilleFood.Domain.MenuDefinition;
 using ObjectvilleDiner.Domain.MenuDefinition;
 using ObjectvilleCafe.Domain.MenuDefinition;
+using ObjectvilleFood.Domain.Visitors;
 
 namespace ObjectvilleFood.Kiosk;
 
@@ -24,16 +25,44 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    var allMenus = new Menu(name: "ALL MENUS", description: "All menus, combined");
-    var dinerMenu = new DinerMenu();
+    var input = "";
+    var currentView = "menu";
+    var healthScoreVisitor = new HealthScoreVisitor();
 
-    dinerMenu.Add(new DinerDessertMenu());
-    allMenus.Add(new PancakeHouseMenu());
-    allMenus.Add(dinerMenu);
-    allMenus.Add(new CafeMenu());
+    while (input != "q!")
+    {
+      if (input == "n") currentView = "healthScoreInfo";
 
-    var waitress = new Waitress(allMenus);
+      if (currentView == "menu")
+      {
+        Console.Clear();
 
-    waitress.PrintMenu();
+        var allMenus = new Menu(name: "ALL MENUS", description: "All menus, combined");
+        var dinerMenu = new DinerMenu();
+
+        dinerMenu.Add(new DinerDessertMenu());
+        allMenus.Add(new PancakeHouseMenu());
+        allMenus.Add(dinerMenu);
+        allMenus.Add(new CafeMenu());
+        allMenus.Accept(healthScoreVisitor);
+        var waitress = new Waitress(allMenus);
+
+        waitress.PrintMenu();
+
+        Console.WriteLine("");
+        Console.WriteLine("- Type 'n' for health score info");
+        Console.WriteLine("- Type 'q!' to quit");
+      }
+      else if (currentView == "healthScoreInfo")
+      {
+        Console.WriteLine("Health score info:");
+        healthScoreVisitor.GetHealthInfoInfo().ForEach(healthInfo =>
+        {
+          Console.WriteLine($"{healthInfo.MenuItemName}: {healthInfo.Score}");
+        });
+      }
+
+      input = Console.ReadLine();
+    }
   }
 }
