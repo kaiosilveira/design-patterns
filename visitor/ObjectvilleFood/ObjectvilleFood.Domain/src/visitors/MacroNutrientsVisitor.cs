edb4bf93dca10817207d572b4dceb24379cd7777
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-28 09:39:52 +0100

    visitor: ObjectvilleFood - add item name to MacroNutrientInfo

--- visitor/ObjectvilleFood/ObjectvilleFood.Domain/src/visitors/MacroNutrientsVisitor.cs
+++ visitor/ObjectvilleFood/ObjectvilleFood.Domain/src/visitors/MacroNutrientsVisitor.cs
@@ -5,12 +5,14 @@ namespace ObjectvilleFood.Domain.Visitors;
 
 public class MacroNutrientsInfo
 {
+  public string MenuItemName { get; private set; }
   public double ProteinInGrams { get; private set; }
   public double CarbohydratesInGrams { get; private set; }
   public double FatInGrams { get; private set; }
 
-  public MacroNutrientsInfo(double proteinInGrams, double carbohydratesInGrams, double fatInGrams)
+  public MacroNutrientsInfo(string itemName, double proteinInGrams, double carbohydratesInGrams, double fatInGrams)
   {
+    this.MenuItemName = itemName;
     this.ProteinInGrams = proteinInGrams;
     this.CarbohydratesInGrams = carbohydratesInGrams;
     this.FatInGrams = fatInGrams;
@@ -31,6 +33,7 @@ public class MacroNutrientsVisitor : Visitor
   public void VisitMenuItem(MenuItem item)
   {
     var info = new MacroNutrientsInfo(
+      item.Name,
       item.ProteinInGrams,
       item.CarbohydratesInGrams,
       item.FatInGrams
